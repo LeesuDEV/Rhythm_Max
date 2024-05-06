9c07394bfb629a2b18898ddb91005b88f0853df1
Suhyun Lee <tngus9753141@gmail.com>
2024-05-06 21:59:26 +0900

    Limbo 추가

--- app/src/main/java/com/example/rhythmproto/MainActivity.java
+++ app/src/main/java/com/example/rhythmproto/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -18,21 +20,91 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout odysseus;
+    LinearLayout Xeus;
+    LinearLayout Limbo;
 
     String songName;
     String songDifficulty;
     String songBPM;
     int songImage; //곡 이미지
+    int song_mp3; //노래 파일
     int noteData; //채보데이터
     int difficultySet = 1;  //  1은 easy 2는 hard
     String selectSong = ""; // 선택된 곡
+    Switch clapSoundSwitch; // 타격음 소리 스위치
+    Button setSpeedBtn; //배속설정 버튼
+    static float setSpeed = 3000; // 배속설정
+    static int speedIndex = 1; // 배속모드 식별값
+    static float setSpeedJudgment = 1; // 배속모드에 따른 판정 배율값
 
+    //1배속 = 3000ms , 1.5배속 = 2000ms , 2배속 = 1500ms , 2.5배속 = 1200ms, 3배속 = 1000ms, 3.5배속 = 857ms
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         odysseus = findViewById(R.id.odysseus);
+        Xeus = findViewById(R.id.Xeus);
+        Limbo = findViewById(R.id.Limbo);
+        clapSoundSwitch = findViewById(R.id.ClapSoundSwitch);
+        setSpeedBtn = findViewById(R.id.speedSetBtn);
+
+        setDefaultSpeed(speedIndex); //화면 구성시 초기 인덱스값(혹은 db에서 받아온 유저의 배속값)을 버튼+배속에 설정
+
+        setSpeedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (speedIndex) {
+                    case 1 :
+                        speedIndex = 2; // 배속 식별값을 2로변경 (1.5배 식별값)
+                        setSpeed = 2000; // 1.5배속 값을 세팅
+                        setSpeedJudgment = 1.5f; // 1.5배율을 세팅
+                        setSpeedBtn.setText("1.5X"); //버튼 텍스트를 1.5배속으로 변경
+                        break;
+                    case 2 :
+                        speedIndex = 3;
+                        setSpeed = 1500;
+                        setSpeedJudgment = 2f;
+                        setSpeedBtn.setText("2X");
+                        break;
+                    case 3 :
+                        speedIndex = 4;
+                        setSpeed = 1200;
+                        setSpeedJudgment = 2.5f;
+                        setSpeedBtn.setText("2.5X");
+                        break;
+                    case 4 :
+                        speedIndex = 5;
+                        setSpeed = 1000;
+                        setSpeedJudgment = 3;
+                        setSpeedBtn.setText("3X");
+                        break;
+                    case 5 :
+                        speedIndex = 6;
+                        setSpeed = 857;
+                        setSpeedJudgment = 3.5f;
+                        setSpeedBtn.setText("3.5X");
+                        break;
+                    case 6 :
+                        speedIndex = 1;
+                        setSpeed = 3000;
+                        setSpeedJudgment = 1;
+                        setSpeedBtn.setText("1X"); // 배속으로 다시 변경
+                        break;
+                }
+            }
+        });  // 배속설정버튼
+
+        clapSoundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    SoundManager.getInstance().loadSound(MainActivity.this); // 판정타격음 스위치를 켰다면 타격소리를 로딩
+                } else {
+                    SoundManager.instance = null;
+                }
+            }
+        });
 
         odysseus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,18 +113,75 @@ public class MainActivity extends AppCompatActivity {
             }
         }); //오디세우스 레이아웃 클릭시
 
-        SoundManager.getInstance().loadSound(this); //판정 드럼소리 로딩
+        Xeus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectSong(Xeus);  // 곡선택 로직 동작후 커스텀다이어로그 생성
+            }
+        }); //제우스 레이아웃 클릭시
+
+        Limbo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectSong(Limbo);  // 곡선택 로직 동작후 커스텀다이어로그 생성
+            }
+        });
     }
+
+    public void setDefaultSpeed(int speedIndex) {
+       switch (speedIndex){
+           case 1 :
+               setSpeed = 3000; // 1배속 값을 세팅
+               setSpeedJudgment = 1; // 1배율을 세팅
+               setSpeedBtn.setText("1X"); //버튼 텍스트를 1배속으로 변경
+               break;
+           case 2 :
+               setSpeed = 2000;
+               setSpeedJudgment = 1.5f;
+               setSpeedBtn.setText("1.5X");
+               break;
+           case 3 :
+               setSpeed = 1500;
+               setSpeedJudgment = 2f;
+               setSpeedBtn.setText("2X");
+               break;
+           case 4 :
+               setSpeed = 1200;
+               setSpeedJudgment = 2.5f;
+               setSpeedBtn.setText("2.5X");
+               break;
+           case 5 :
+               setSpeed = 1000;
+               setSpeedJudgment = 3f;
+               setSpeedBtn.setText("3X");
+               break;
+           case 6 :
+               setSpeed = 857;
+               setSpeedJudgment = 3.5f;
+               setSpeedBtn.setText("3.5X");
+               break;
+       }
+    } //화면 구성시 초기 인덱스값(혹은 db에서 받아온 유저의 배속값)을 버튼+배속에 설정
 
     public void selectSong(LinearLayout song) {
         switch (song.getId()){
             case R.id.odysseus :
-                selectSong = "odysseus";
+                selectSong = "odysseus";  // 선택된곡 String으로 난이도 조절 메소드에서 곡정보를 인식
                 odysseus(); // 곡정보를 odysseus로 설정
                 showCustomDialog();
                 break;
+            case R.id.Xeus :
+                selectSong = "xeus";  // 선택된곡 String으로 난이도 조절 메소드에서 곡정보를 인식
+                xeus(); // 곡정보를 xeus로  설정
+                showCustomDialog();
+                break;
+            case R.id.Limbo :
+                selectSong = "limbo";  // 선택된곡 String으로 난이도 조절 메소드에서 곡정보를 인식
+                limbo(); // 곡정보를 xeus로  설정
+                showCustomDialog();
+                break;
         }
-    }
+    } //레이아웃 이름을 가지고와서 곡정보를 세팅하고 다이어로그를 띄움
 
     public void showCustomDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
@@ -97,6 +226,7 @@ public class MainActivity extends AppCompatActivity {
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*----------------------------보낼 인턴트 값 모음 시작------------------------*/
                 List<NoteData> notes = OsuFileParser.parseOsuFile(MainActivity.this, noteData);  // 선택된곡 노트데이터를 리스트에 삽입
                 Intent intent = new Intent(MainActivity.this, GameActivity.class); // 인턴트생성
                 intent.putParcelableArrayListExtra("notes", new ArrayList<>(notes)); // 채보리스트를 인턴트에 담아서 전송
@@ -104,8 +234,12 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("songName",songName);  // 곡이름 인턴트전송
                 intent.putExtra("songDifficulty",songDifficulty);  // 곡 난이도 인턴트전송
                 intent.putExtra("songBpm",songBPM);  // 곡 bpm 인턴트전송
+                intent.putExtra("song_mp3",song_mp3);  // 곡 노래mp3 인턴트 전송
+                intent.putExtra("setSpeed",setSpeed);  // 배속설정 인턴트 전송
+                intent.putExtra("setSpeedJudgment",setSpeedJudgment); // 배율을 인턴트로 전송
                 startActivity(intent);
                 dialog.dismiss();
+                /*----------------------------보낼 인턴트 값 모음 끝------------------------*/
             }
         });
         cancelBtn.setOnClickListener(new View.OnClickListener() {
@@ -122,17 +256,57 @@ public class MainActivity extends AppCompatActivity {
             songImage = R.drawable.odysseus_img;  // 오디세우스 이미지설정
             songBPM = "187";  // 오디세우스 bpm
             songName = "Odysseus[EASY]";  // 오디세우스 곡제목
-            songDifficulty = "★★★★★";  // 오디세우스 난이도
-            noteData = R.raw.odysseus;
+            songDifficulty = "★★★★★★";  // 오디세우스 난이도
+            noteData = R.raw.odysseus; // 오디세우스 채보 데이터
+            song_mp3 = R.raw.xeon; // 오디세우스 mp3파일
         }
         if (difficultySet == 2) {
-            songImage = R.drawable.odysseus_img;  // 오디세우스 이미지설정
-            songBPM = "187";  // 오디세우스 bpm
-            songName = "Odysseus[HARD]";  // 오디세우스 곡제목
-            songDifficulty = "★★★★★★★★";  // 오디세우스 난이도
-            noteData = R.raw.odysseus;
+            songImage = R.drawable.odysseus_img;
+            songBPM = "187";
+            songName = "Odysseus[HARD]";
+            songDifficulty = "★★★★★★★★";
+            noteData = R.raw.odysseus_hard;
+            song_mp3 = R.raw.xeon;
         }
     }  // 오디세우스 곡 정보 설정 메소드
+
+    public void xeus(){
+        if (difficultySet == 1) {
+            songImage = R.drawable.xeus_img;
+            songBPM = "148";
+            songName = "Xeus[EASY]";
+            songDifficulty = "★★★★★★";
+            noteData = R.raw.xeus_easy;
+            song_mp3 = R.raw.xeus;
+        }
+        if (difficultySet == 2) {
+            songImage = R.drawable.xeus_img;
+            songBPM = "148";
+            songName = "Xeus[HARD]";
+            songDifficulty = "★★★★★★★★★";
+            noteData = R.raw.xeus_hard ;
+            song_mp3 = R.raw.xeus;
+        }
+    }  // 제우스 곡 정보 설정 메소드
+
+    public void limbo(){
+        if (difficultySet == 1) {
+            songImage = R.drawable.limbo_img;
+            songBPM = "191.9";
+            songName = "Limbo[EASY]";
+            songDifficulty = "★★★★★★";
+            noteData = R.raw.limbo_hard;
+            song_mp3 = R.raw.limbo;
+        }
+        if (difficultySet == 2) {
+            songImage = R.drawable.limbo_img;
+            songBPM = "191.9";
+            songName = "Limbo[HARD]";
+            songDifficulty = "★★★★★★★★★";
+            noteData = R.raw.limbo_hard ;
+            song_mp3 = R.raw.limbo;
+        }
+    }  // 제우스 곡 정보 설정 메소드
 
     public void changeDialogInfo(ImageView songImageView,TextView songNameTV,TextView difficultyTV,TextView bpmTV){
         songImageView.setImageResource(songImage);  // 선택된곡의 이미지 세팅
@@ -147,6 +321,15 @@ public class MainActivity extends AppCompatActivity {
             case "odysseus" :
                 odysseus();
                 changeDialogInfo(songImageView,songNameTV,difficultyTV,bpmTV); // 다이어로그 정보 변경
+                break;
+            case "xeus" :
+                xeus();
+                changeDialogInfo(songImageView,songNameTV,difficultyTV,bpmTV); // 다이어로그 정보 변경
+                break;
+            case "limbo" :
+                limbo();
+                changeDialogInfo(songImageView,songNameTV,difficultyTV,bpmTV); // 다이어로그 정보 변경
+                break;
         }
     }
 
---
+++ app/src/main/java/com/example/rhythmproto/NoteManager-leesu.java
@@ -0,0 +1,122 @@
+package com.example.rhythmproto;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Handler;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteManager {
+    private Context context;
+    private ViewGroup noteLayout; // 노트가 표시될 레이아웃
+    private List<NoteView> notes = new ArrayList<>(); // 생성된 노트 뷰들을 관리하는 리스트
+    public Handler handler = new Handler();
+    public static List<NoteView>[] lanes = new List[5];  // 5개의 레인을 위한 배열
+
+    public NoteManager(Context context, ViewGroup noteLayout) {
+        this.context = context;
+        this.noteLayout = noteLayout;
+        this.handler = new Handler(); // 핸들러 초기화
+
+        for (int i = 0; i < lanes.length; i++) {
+            lanes[i] = new ArrayList<>();
+        }
+    } // 노트매니저 초기화값
+
+    public void createNotesFromData(List<NoteData> notesData) {
+        long gameStartTime = System.currentTimeMillis();
+
+        for (NoteData data : notesData) {
+            long delay = data.time - (System.currentTimeMillis() - gameStartTime);  // 지연 시간 계산
+            if (delay < 0) {
+                continue; // 과거 시간에 대한 노트는 처리하지 않음
+            }
+            handler.postDelayed(() -> {
+                createNoteView(data);
+            }, delay);
+        }
+    }  // 레인별 노트 데이터를 지연시간을 계산하여 대기 큐에 올린후, 차례가되면 해당 데이터를 가지고 노트출력을 시작하는 메소드
+
+    private void createNoteView(NoteData data) {
+        // 노트 라인 위치 계산 (x 값에 따라)
+        float startX = calculateXPosition(data.x);
+        int index = getLaneIndex(data.x);  // 라인구별을 위한 Index값
+        NoteView noteView = new NoteView(context, startX, 0, Color.RED,noteLayout.getHeight(),noteLayout,index);  // 초기 y 위치는 0
+        noteLayout.addView(noteView);  // noteLayout은 노트를 포함할 레이아웃의 ID
+        notes.add(noteView);  // 관리 목록에 노트 뷰 추가
+        // 노트를 내리는 로직 시작
+        startFallingAnimation(noteView, noteLayout.getHeight(), (int) GameActivity.setSpeed); // 노트 애니메이션 시작
+        assignNoteToLane(data.x,noteView);  //data.x (2번레인 = 103)값을 매개변수로 lane을 구별하여 각 lane의 List<>에 데이터 삽입
+    }  // 노트의 파싱데이터로 각 노트를 레인을 구분하여 레인에 데이터를 넣어주고. 노트의 떨어지는 애니메이션 시작을 선언하는 메소드
+
+    private void startFallingAnimation(NoteView noteView, int endY, int duration) {
+        noteView.startFalling(noteView,endY, duration);
+    } // 노트의 떨어지는 애니메이션
+
+
+    private float calculateXPosition(int x) {
+        // x 좌표 값에 따라 레인 인덱스를 결정
+        int laneIndex = getLaneIndex(x);
+        int layoutWidth = noteLayout.getWidth();
+        float laneWidth = layoutWidth / 4.0f;  // 5개 레인을 가정 165가 나와야함 4개레인 230.25
+        float returnValue = (laneWidth * laneIndex);
+        return returnValue;
+    }  // getLaneIndex의 x값으로 float X좌표값을 반환
+
+    private int getLaneIndex(int x) {
+        // 예제 코드, 실제 값에 따라 인덱스를 조정해야 할 수 있습니다.
+        if (x == 0) return 0;
+        else if (x == 103) return 1;
+        else if (x == 205) return 2;
+        else if (x == 308) return 3;
+        else if (x == 410) return 4;
+        return 0;
+    }  // 레인 인덱스값을 반환
+
+    private void assignNoteToLane(int index,NoteView noteView){
+        if (getLaneIndex(index) == 0) {
+            lanes[0].add(noteView);
+        }
+        else if (getLaneIndex(index) == 1) {
+            lanes[1].add(noteView);
+        }
+        else if (getLaneIndex(index) == 2) {
+            lanes[2].add(noteView);
+        }
+        else if (getLaneIndex(index) == 3) {
+            lanes[3].add(noteView);
+        }
+        else if (getLaneIndex(index) == 4) {
+            lanes[4].add(noteView);
+        }
+    }// 레인별 노트를 노트리스트에 담아주는 메소드
+
+    public List<NoteView> getLaneNotes(int laneIndex) {
+        switch (laneIndex) {
+            case 0:
+                return lanes[0];
+            case 1:
+                return lanes[1];
+            case 2:
+                return lanes[2];
+            case 3:
+                return lanes[3];
+            case 4:
+                return lanes[4];
+            default:
+                return null;
+        }
+    }  // GameActivity의 TouchEvent에서 해당 터치버튼의 노트라인 데이터를 반환받고자 할때 사용하는 메소드
+
+    public synchronized void removeNoteFromLane(NoteView note) {
+        for (List<NoteView>lane : lanes) {
+            lane.remove(note);
+        }
+    }  // NoteView내에서 판정됐거나 Miss처리된 객체를 리스트에서 삭제하는 동기화 메소드
+
+    public void shutdownHandler() {
+        handler.removeCallbacksAndMessages(null);  //GameActvity 종료시 예약메세지와 콜백을 삭제하기위함
+    }  // NoteManager 노트생성 핸들러 종료메소드
+}
