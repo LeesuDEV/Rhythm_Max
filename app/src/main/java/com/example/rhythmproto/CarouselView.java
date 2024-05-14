4fb5450c40ed6c5186a2f91c498a8f8c3f91dcae
Suhyun Lee <tngus9753141@gmail.com>
2024-05-15 03:31:56 +0900

    bug fix

---
+++ app/src/main/java/com/example/rhythmproto/CarouselView.java
@@ -0,0 +1,12 @@
+package com.example.rhythmproto;
+
+import android.view.View;
+import android.widget.Gallery;
+
+public class CarouselView extends Gallery {
+
+    public CarouselView(){
+        this.setStaticTransformationsEnabled(true);
+    }
+    
+}
---
+++ app/src/main/java/com/example/rhythmproto/CarouselView.kt
@@ -0,0 +1,5 @@
+package com.example.rhythmproto
+
+class CarouselView {
+    
+}
